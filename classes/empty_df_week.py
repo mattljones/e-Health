abeadeca092c40e66ae76d1d7f24bfeab88322c0
Aleksei Sukhov <aleksei.sukhov.20@ucl.ac.uk>
2020-12-12 17:15:25 +0000

    Cleaned up the code a little bit, now it will create an empty DF with weekends filled, that way for every method you can populate it from the DB

--- classes/empty_df_week.py
+++ classes/empty_df_week.py
@@ -25,23 +25,34 @@ def db_read_query(query):
 
 
 """
-week_empty_df function creates an empty dataframe that can get populated
-using .loc pinpointing the date and time from the database
+week_empty_df function creates an empty dataframe that already contains days when GP has their weekends.
+It can get populated using .loc pinpointing the date and time of bookings that we get from the database.
+This function return a DataFrame without tabulate formatting  
 """
 
 
-def week_empty_df(start_date):
+def week_empty_df(start_date,gp_id):
     days = pd.date_range(start=start_date, periods=7, freq='D')
     times = pd.date_range(start='08:00:00', periods=54, freq='10Min')  # .to_frame(name='Working Hours',index=False)
-    df = pd.DataFrame(index=times.time, columns=days.date)
-    return df
+    week_df = pd.DataFrame(index=times.time, columns=days.date)
+
+    working_day_query = """SELECT gp_working_days FROM gp where gp_id == {};""".format(gp_id)
+    working_day = db_read_query(working_day_query).loc[0, 'gp_working_days']  # will need a query to pull the first working day for a specific GP
+
+    # This part of the code works out when the GP has weekends and populates those days with status "Weekend"
+    weekend_day_range = [(working_day + 5) % 7, (working_day + 6) % 7]
+
+    for i in range(7):
+        if week_df.columns[i].weekday() in weekend_day_range:
+            week_df[week_df.columns[i]] = 'Weekend'
+    return week_df.fillna(" ")
 
 
-# This snippet of code fetches the bookings for a week specified
+# This snippet of code fetches the bookings for a week specified and populates the empty week dataframe
 # In your method you will have to dynamically generate the the end date and
 # input in into the SQL query
 
-week_empt_df = week_empty_df('2020-12-08')
+week_empt_df = week_empty_df('2020-12-08',10)
 query = """SELECT date(booking_start_time) start_date,strftime('%H:%M:%S',booking_start_time) time,booking_status
            FROM booking
            WHERE gp_id = 10 AND start_date BETWEEN '{}' and '{}';""".format('2020-12-08', '2020-12-15')
@@ -53,15 +64,4 @@ for i in range(sql_result_df.shape[0]):
     time_row = dt.datetime.strptime(sql_result_df.loc[i, 'time'], '%H:%M:%S').time()
     week_empt_df.loc[time_row, date_column] = sql_result_df.loc[i, 'booking_status']
 
-#####################################
-working_day = 0  # will need a query to pull the first working day for a specific GP
-
-# This part of the code works out when the GP has weekends and populates those days with status "Weekend"
-weekend_day_range = [(working_day + 5) % 7, (working_day + 6) % 7]
-print(weekend_day_range)
-for i in range(7):
-    if week_empt_df.columns[i].weekday() in weekend_day_range:
-        week_empt_df[week_empt_df.columns[i]] = 'Weekend'
-
-week_empt_df = tabulate(week_empt_df.fillna(" "), headers='keys', tablefmt='psql')
-print(week_empt_df)
+print(schedule_df)
