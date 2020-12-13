6f6ffb5146c5640781496f6945fe812dce7ba2e7
tomjfsmith <thomas.jf.smith@gmail.com>
2020-12-13 18:49:04 +0000

    Function skeletons

--- user_menu_flow/admin_flow.py
+++ user_menu_flow/admin_flow.py
@@ -17,6 +17,144 @@ from system import globals
 
 ############################### INPUT MENU PAGES ###########################
 
+###### MANAGE GP ACCOUNTS FUNCTIONS ######
+
+def view_gp(next_dict):
+    '''
+    Select a list of GPs and allows choice for viewing.
+    '''
+    pass
+
+def add_gp(next_dict):
+    '''
+    Adds a new GP.
+    '''
+    pass
+
+def deactivate_gp(next_dict):
+    '''
+    Deactivates a GP.
+    '''
+    pass
+
+def delete_gp(next_dict):
+    '''
+    Deletes a GP.
+    '''
+    pass
+
+###### MANAGE PATIENT ACCOUNTS FUNCTIONS ######
+
+def view_patient(next_dict):
+    '''
+    View a Patient Account.
+    '''
+    pass
+
+def confirm_patient(next_dict):
+    '''
+    Confirm pending patient registrations.
+    '''
+    pass
+
+def delete_patient(next_dict):
+    '''
+    Delete a patient account
+    '''
+    pass
+
+###### MANAGE GP-PATIENT PAIRINGS FUNCTIONS ######
+
+def pairing_gp(next_dict):
+    '''
+    Search for a patient and pair them up with a GP.
+    '''
+    pass
+
+def pairing_patient(next_dict):
+    '''
+    Select a GP and pair patients to them if they are not full.
+    '''
+    pass
+
+###### MANAGE GP SCHEDULES FUNCTIONS ######
+
+def view_scheule(next_dict):
+    '''
+    View a GP's current schedule.
+    '''    
+    pass
+
+def manage_availability(next_dict):
+    '''
+    Manage the availability of GPs.
+    '''
+    pass
+
+def modify_availability(next_dict):
+    '''
+    Change a GP's availability.
+    '''
+    pass
+
+def manage_time_off(next_dict):
+    '''
+    Manage a GP's time off.
+    '''
+    pass
+
+def view_time_off(next_dict):
+    '''
+    View a GP's current time off.
+    '''
+    pass
+
+def add_time_off(next_dict):
+    '''
+    Add time off to a GP's schedule.
+    '''
+    pass
+
+def remove_time_off(next_dict):
+    '''
+    Remove time off from a GP's schedule.
+    '''
+    pass
+
+###### MANAGE UPCOMING APPOINTMENTS FUNCTIONS ######
+
+def add_appointment(next_dict):
+    '''
+    Schedule an appointment for a GP.
+    '''
+    pass
+
+def view_appointment(next_dict):
+    '''
+    View a current list of appointments.
+    '''
+    pass
+
+def appointment_by_patient(next_dict):
+    '''
+    Find a patient's upcoming appointments.
+    '''
+    pass
+
+def appointment_by_gp(next_dict):
+    '''
+    Find a GP's upcoming appointments with date options.
+    '''
+    pass
+
+
+###### APPOINTMENT SUMMARIES FUNCTIONS ######
+
+def appointment_summary_date(next_dict):
+    '''
+    Returns the appointment summaries based on the user choice.
+    '''
+    pass
 
 ############################ SEQUENTIAL STEPS MENUS ########################
 
@@ -61,7 +199,6 @@ add_new_gp_account_final_menu = {
     "2": ("Section Menu", , manage_gp_accounts_flow)
 }
 
-#View/Edit GP Accounts Sub-menu
 view_edit_gp_accounts_final_menu = {
     "title": "NEXT ACTIONS",
     "type": "sub",
@@ -70,7 +207,6 @@ view_edit_gp_accounts_final_menu = {
     "3": ("Section Menu", , manage_gp_accounts_flow)
 }
 
-#
 manage_gp_accounts_flow = {
     "title": "MANAGE GP ACCOUNTS",
     "type": "sub",
@@ -291,19 +427,20 @@ manage_appointment_flow = {
 }
 
 ###### VIEW APPOINTMENT SUMMARIES SUB-MENU ######
+
 appointment_summary_final_actions = {
     "title": "NEXT ACTIONS",
     "type": "sub",
-    "1": ("View a Different Appointment", , appointment_summary_date_flow),
+    "1": ("View a Different Appointment", appointment_summary_date, empty_dict),
     "2": ("View a Different Patient", , ),
 }
 
 appointment_summary_date_flow = {
     "title": "SELECT APPOINTMENT DATE",
     "type": "sub",
-    "1": ("Day", ,),
-    "2": ("Week", ,),
-    "3": ("Custom", ,)
+    "1": ("Day", appointment_summary_date, appointment_summary_final_actions),
+    "2": ("Week", appointment_summary_date, appointment_summary_final_actions),
+    "3": ("Custom", appointment_summary_date, appointment_summary_final_actions)
 }
 
 ###### MAIN MENU ####
@@ -311,10 +448,10 @@ appointment_summary_date_flow = {
 main_flow = {
     "title": "ADMIN MAIN MENU",
     "type":"main",
-    "1":("Manage GP Accounts", , manage_gp_accounts_flow),
-    "2":("Manage Patient Accounts", ,manage_patient_accounts_flow ),
-    "3":("Manage GP-Patient Pairings", , manage_pairings_flow),
-    "4":("Manage GP Schedules", , manage_schedules_flow),
-    "5":("Manage Upcoming Appointments", , manage_appointments_flow),
-    "6":("View Appointment Summaries", , view_summaries_flow)
+    "1":("Manage GP Accounts", empty_method, manage_gp_accounts_flow),
+    "2":("Manage Patient Accounts", empty_method, manage_patient_accounts_flow ),
+    "3":("Manage GP-Patient Pairings", empty_method, manage_pairings_flow),
+    "4":("Manage GP Schedules", empty_method, manage_schedules_flow),
+    "5":("Manage Upcoming Appointments", empty_method, manage_appointments_flow),
+    "6":("View Appointment Summaries", empty_method, view_summaries_flow)
 }
