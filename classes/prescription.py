fd0953b533fe3aff3e001f8a94cb092bf83b4b84
manfredbeluga <manfred.beluga@gmail.com>
2020-12-14 17:53:52 +0000

    updated prescription.py

--- classes/prescription.py
+++ classes/prescription.py
@@ -6,6 +6,23 @@ import sqlite3 as sql
 import datetime
 
 
+def db_execute(query):
+    conn = sql.connect('database/db_comp0066.db')
+    c = conn.cursor()
+    c.execute(query)
+    # Commit to db
+    conn.commit()
+    print("Info successfully committed")
+    # Close db
+    conn.close()
+
+
+def db_read_query(query):
+    conn = sql.connect("database/db_comp0066.db")
+    result = pd.read_sql_query(query, conn)
+    conn.close()
+    return result
+
 class Prescription:
     '''
     Class defining all 'prescription' related methods.
@@ -13,7 +30,7 @@ class Prescription:
 
     def __init__(self):
         self.prescription_id = ""
-        self.prescription_timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.prescription_timestamp = ""
         self.prescription_expiry_date = ""
         self.drug_id = ""
         self.drug_dosage = ""
@@ -23,21 +40,21 @@ class Prescription:
     def insert(self):  # INSERT - INSTANCE
         '''
         Insertion of a prescription into the prescription table
-        :return: none --> prescription is inserted in prescription table
+        :return: 'Prescription is inserted in prescription table'
         '''
-        conn = sql.connect("database/db_comp0066.db")
-        c = conn.cursor()
-        c.execute("INSERT INTO prescription VALUES (NULL, ?, ?, ?, ?, ?, ?);",
-                  (self.prescription_timestamp,
-                   self.prescription_expiry_date,
-                   self.drug_id,
-                   self.drug_dosage,
-                   self.drug_frequency_dosage,
-                   self.booking_id)
-                  )
+        insert_query = """
+                        INSERT INTO prescription
+                        VALUES (NULL, '{}', '{}', {}, '{}', '{}', {});""".format(
+                                                                        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                                        self.prescription_expiry_date,
+                                                                        self.drug_id,
+                                                                        self.drug_dosage,
+                                                                        self.drug_frequency_dosage,
+                                                                        self.booking_id)
 
-        conn.commit()
-        conn.close()
+        db_execute(insert_query)
+
+        return 'Prescription is inserted in prescription table'
 
     @staticmethod  # SELECT_list - STATIC
     def select_drug_list():
@@ -45,15 +62,12 @@ class Prescription:
         Static Method that returns a list of the drug table
         :return: df_drug_list in tabulate format
         '''
-        conn = sql.connect("database/db_comp0066.db")
 
-        query = '''
-                    SELECT drug_id AS "ID", drug_name AS "Drug Name"
-                    FROM drug'''
+        select_drug_query = '''
+                            SELECT drug_id AS "Drug ID", drug_name AS "Drug Name"
+                            FROM drug'''
 
-        df_drug_list = pd.read_sql_query(query, conn)
-
-        conn.close()
+        df_drug_list = db_read_query(select_drug_query)
 
         df_formatted = df_drug_list.to_markdown(tablefmt="grid", index=True)
 
@@ -66,20 +80,18 @@ class Prescription:
         :param patient_for_prescription: patient_id from database
         :return:
         '''
-        conn = sql.connect("database/db_comp0066.db")
-
-        query = '''
-                    SELECT prescription_timestamp AS "Prescription Creation Date", prescription_expiry_date AS "Prescription Expiry Date", drug_name AS "Drug Name", drug_dosage AS "Drug Dosage", drug_frequency_dosage AS "Intake Frequency"
-                    FROM prescription AS p
-                    LEFT JOIN booking AS b ON p.booking_id = b.booking_id
-                    LEFT JOIN drug AS d ON p.drug_id = d.drug_id
-                    WHERE patient_id = {}
-                    AND booking_status = "confirmed"'''.format(patient_id)
 
 
+        select_patient_query = '''
+                                    SELECT prescription_expiry_date AS "Expiry Date", drug_name AS "Drug Name", drug_dosage AS "Drug Dosage",
+                                    drug_frequency_dosage AS "Intake Frequency", p.booking_id AS "Booking ID"
+                                    FROM prescription AS p
+                                    LEFT JOIN booking AS b ON p.booking_id = b.booking_id
+                                    LEFT JOIN drug AS d ON p.drug_id = d.drug_id
+                                    WHERE patient_id = {}
+                                    AND booking_status = "confirmed"'''.format(patient_id)
 
-        df_patient_prescription = pd.read_sql_query(query, conn)
-        conn.close()
+        df_patient_prescription = db_read_query(select_patient_query)
 
         df_formatted = df_patient_prescription.to_markdown(tablefmt="grid", index=True)
 
@@ -88,22 +100,29 @@ class Prescription:
 
 ### TESTING ###
 ## testing prescription
-# call classes
-new_prescription = Prescription()
-#see patient records
-new_prescription.select_patient(22)
-# see drug list
-new_prescription.select_drug_list()
+# # call classes
+# new_prescription = Prescription()
+# # see patient records
+# new_prescription.select_patient(22)
+# # see drug list
+# df = new_prescription.select_drug_list()[0]
 
 
 ## Insert new prescription
 # ask user to input the expiry date
-new_prescription.prescription_expiry_date = input("Enter the expiry date (Format: YYYY-MM-DD): ")
-new_prescription.drug_id = int(input("Enter the drug id: "))
-new_prescription.booking_id = int(input("Enter the booking id: "))
-new_prescription.drug_dosage = input("Enter the drug dosage: ")
-new_prescription.drug_frequency_dosage = input("Enter the intake frequency: ")
-new_prescription.insert()
+# new_prescription.prescription_expiry_date = input("Enter the expiry date (Format: YYYY-MM-DD): ")
+# new_prescription.drug_id = int(input("Enter the drug id: "))
+# new_prescription.booking_id = int(input("Enter the booking id: "))
+# new_prescription.drug_dosage = input("Enter the drug dosage: ")
+# new_prescription.drug_frequency_dosage = input("Enter the intake frequency: ")
+# new_prescription.insert()
+#
+# new_prescription.prescription_expiry_date = '2020-12-12'
+# new_prescription.drug_id = 1
+# new_prescription.booking_id = 2
+# new_prescription.drug_dosage = '12 mg'
+# new_prescription.drug_frequency_dosage = 'hallo'
+# new_prescription.insert()
 
 ### DEVELOPMENT ###
 
