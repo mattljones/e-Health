ebf151b94acc767e9a1999f93b9eb5add9458747
Theo Reignier <theo.reignier.17@ucl.ac.uk>
2020-12-15 15:45:35 +0100

    Error handling and input validation for login and register

--- user_menu_flow/register_login_flow.py
+++ user_menu_flow/register_login_flow.py
@@ -29,58 +29,72 @@ def empty_method(next_dict):
 
 ################################ INPUT MENU PAGES ###########################
 
-# TODO: Error handling, input validation and call corresponding register() function instead of hardcoding
 def login_page(login_as):
     '''
     Function defining the user login page takes an empty 
     dictionary as argument for utils.display function consistency.
+
+    Parameter:
+        - login_as: user type the user is trying to login as selected on previous menu page.
     '''
     print("\n---------------------------------------------------- \n"
           "                    LOGIN\n"
-          "\nPlease, enter your credentials:"
-          "\n  - enter '#' to go back to main page")
+          "\nPlease, enter your credentials"
+          "\nor enter '#' to go back to main page")
 
-
+    # Login details
     login_credentials = ["Email address", "Password"]
-    usr_input = ["",""]
+    usr_input = []
 
-    for i in (0,1):
-        usr_input[i] = input("\n--> " + login_credentials[i] + ": ")
+    # Login input entries
+    i = 0
+    while i < len(login_credentials):
+        single_input = input("\n--> " + login_credentials[i] + ": ")
         
-        if (usr_input[i] == '#'):
+        # Go back to main registration page
+        if (single_input == '#'):
             return utils.display(main_flow_register)
 
-        utils.validate(usr_input[i])
+        # Input validation for email
+        elif (login_credentials[i] == "Email address") and (utils.validate_email(single_input)):
+            usr_input.append(single_input)
 
-    # Hardcoded for now
-    #########################
-    success = True
-    ########################
+        # Input validation for password
+        elif (login_credentials[i] == "Password") and (utils.validate_password(single_input)):
+            usr_input.append(single_input)
 
+        # If invalid input
+        else :
+            i -= 1
+        
+        i += 1
+
+    # Calling the login() method to check user input against database
+    # Returns boolean variable (True = valid credentials; False = invalid credentials)
+    success = utils.login(usr_input[0],usr_input[1],login_as)
+
+    # Invalid login credentials
     if (success == False):
-
-        print("\nInvalid credentials, please try again.")
+        print("\n\U00002757 Invalid credentials, please try again.")
         return login_page(login_as)
     
+    # Valid login credentials
     else:
-        print("\nSuccessful login !")
-        
-        # Hardcoded for now
-        #########################
-        globals.usr_type,globals.usr_id = login_as, 3
-        #########################
+        print("\n\U00002705 Successful login !")
 
+        # Redirects to patient main page 
         if (globals.usr_type == 'patient'):
             return utils.display(patient_flow.main_flow_patient)
 
+        # Redirects to gp main page 
         elif (globals.usr_type == 'gp'):
             return utils.display(gp_flow.main_flow_gp)
 
+        # Redirects to admin main page 
         else:
             return utils.display(admin_flow.main_flow_admin)
 
 
-# TODO: Error handling, input validation and call corresponding register() function instead of hardcoding
 def register_page(next_dict):
     '''
     Function defining the user register page, takes an empty 
@@ -88,32 +102,99 @@ def register_page(next_dict):
     '''
     print("\n---------------------------------------------------- \n"
         "                   REGISTER\n"
-        "\nPlease, fill in the following form:"
-        "\n(enter '#' to go back to main page)")
+        "\nPlease, fill in the following form"
+        "\nor enter '#' to go back to main page")
 
-    usr_details=["First name","Last name", "Email address"]
+    # register gender dictionary
+    gender = {"title": "Gender",
+              "1":"male",
+              "2":"female",
+              "3":"not applicable"}
+    
+    # register NHS blood donor dictionary
+    blood_donor = {"title": "NHS Blood donor",
+                   "1":"yes",
+                   "2":"no"}
+
+    # register NHS blood donor dictionary
+    organ_donor = {"title": "NHS Organ donor",
+                   "1":"yes",
+                   "2":"no"}
+
+    # Registration details
+    usr_details=["First name", "Last name", "Birth date (YYYY-MM-DD)", "Email address", "Password", "Gender", "NHS blood donor", "NHS organ donor"]
+    MCQ_details_dict = [gender,blood_donor,organ_donor]
     usr_input=[]
-
-    for i in usr_details:
-        single_input = input("\n--> " + i + ": ")
-
-        if single_input == "#" :
+    
+    # Registration String entries
+    i = 0
+    while i < len(usr_details) - 3:
+        single_input = input("\n--> " + usr_details[i] + ": ")
+        
+        # Go back to mian registration page 
+        if (single_input == '#'):
             return utils.display(main_flow_register)
 
-        else:
+        # Input validation for email
+        elif (usr_details[i] == "Email address") and (utils.validate_email(single_input)):
             usr_input.append(single_input)
+
+        # Input validation for password 
+        elif (usr_details[i] == "Password") and (utils.validate_password(single_input)):
+            usr_input.append(single_input)
+
+        # Input validation for password 
+        elif (usr_details[i] == "Birth date (YYYY-MM-DD)") and (utils.validate_date(single_input)):
+            usr_input.append(single_input)
+
+        # Input validation for the other registration details
+        elif (usr_details[i] not in ("Email address","Password", "Birth date (YYYY-MM-DD)")) and (utils.validate(single_input)):
+            usr_input.append(single_input)
+
+        # If invalid input
+        else :
+            i -= 1
+        
+        i += 1
+
+    # Registration MCQ entries
+    i = 0
+    while i < len(MCQ_details_dict):
+        print("\n------------------")
+        print(MCQ_details_dict[i]["title"])
+
+        # Print user options and corresponding keys
+        for key in MCQ_details_dict[i]:
+            if key != "title":
+                print("  [", key, "] " + MCQ_details_dict[i][key])
+
+        # User input 
+        usr_choice = input("\n--> ") 
+
+        # If valid input, store in usr_input
+        if usr_choice in MCQ_details_dict[i]:
+            usr_input.append(MCQ_details_dict[i][usr_choice])
+
+        # If invalid input
+        else:
+            print("\n\U00002757 Invalid entry, please try again")
+            i -= 1
+        
+        i += 1
     
-    # Hardcoded for now
+    # TODO: Hardcoded for now
     ###########################
-    success = False
+    success = True
     ###########################
 
-    if (success == True):
-        print("\nInvalid entry, please try again.")
+    # If problem with registration
+    if (success == False):
+        print("\n\U00002757 Your registration could not be completed due to an unknown issue.")
         return register_page(next_dict)
 
+    # If registration successful
     else :
-        print("\nYour registration must now be approved. \nYou will have to wait for a moment before being able to login.")
+        print("\n\U00002705 Your registration must now be approved. \nYou will have to wait for a moment before being able to login.")
         return utils.display(main_flow_register)
 
 
