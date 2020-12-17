428e6149a80f5cb8ef7f7d1028d91eb069d8f85a
Theo Reignier <theo.reignier.17@ucl.ac.uk>
2020-12-17 13:08:58 +0100

    Manage appointments for patients

--- user_menu_flow/patient_flow.py
+++ user_menu_flow/patient_flow.py
@@ -10,12 +10,109 @@ sys.path.insert(0, str(package_dir))
 
 # Importing utility methods from the 'system' package
 from system import utils
+from classes.appointment import Appointment
 
 # import global variables from globals.py
 from system import globals
 
 # TODO: fill in the function corresponding to user choices (below)
 ############################### INPUT MENU PAGES ###########################
+
+def manage_appointment(next_dict):
+    '''
+    This method works out whether the logged in patient has an upcoming appointment, 
+    had an appointment or neither of those.
+    '''
+
+    # To start with, we assume the user has no appointments booked
+    has_appointment = False
+    had_appointment = False
+
+    # Fetching data frames containing future and past appointments of patient 
+    previous = Appointment.select_patient('previous', globals.usr_id)
+    upcoming = Appointment.select_patient('upcoming', globals.usr_id)
+
+    # Display summary of appointents if patient has book at least one appointment (future or past)
+    if len(previous[0].index) > 0 or len(upcoming[0].index) > 0:
+
+        print ('\n----------------------------------------------------\n'
+              '              APPOINTMENT SUMMARY')
+
+        # If at least one appointment has been booked in the past
+        if len(previous[0].index) > 0:
+            had_appointment = True
+            print("\n-- Past Appointments -- \n" + previous[1])
+        
+        # If at least one appointment has been booked in the future
+        if len(upcoming[0].index) > 0:
+            has_appointment = True
+            print("\n-- Future Appointments -- \n" + upcoming[1])
+
+
+        # If patient has past appointments and upcoming appointemnts 
+        if had_appointment and has_appointment:
+
+            i = ""
+            while i not in ("1","2"):
+
+                print("\n----------------------------------------------------\n"
+                    "            MANAGE WHICH APPOINTMENTS ? \n")
+
+                print('[ 1 ] Previous appointments')
+                print('[ 2 ] Upcoming appointments')
+
+                i = input("\n--> ")
+
+                if i == "1":
+                    return utils.display(next_dict["had"])
+                
+                elif i == "2":
+                    return utils.display(next_dict["has"])
+
+                else:
+                    print("\n\U00002757 Invalid entry, please try again")
+
+        # If patient has upcoming appointemnts only
+        elif has_appointment :
+            return utils.display(next_dict["has"])
+
+        # If patient has past appointemnts only
+        elif had_appointment:
+            return utils.display(next_dict["had"])
+
+    # If patient has not booked appointments yet
+    else:
+        return utils.display(next_dict["has_not"])
+
+
+def cancel_appointment(next_dict):
+    '''
+    Method corresponding to user choice of cancelling upcoming appointment.
+    '''
+
+    print("\n----------------------------------------------------\n"
+            "            CANCEL WHICH APPOINTMENT ? \n")
+
+    # Print a reminder of upcoming appointments
+    upcoming = Appointment.select_patient('upcoming', globals.usr_id)
+    print(upcoming[1])
+
+    print("\n[ 1 ] Cancel 1st row of the table"
+          "\n[ 2 ] Cancel 2nd row of the table"
+          "\n  ... "
+          "\n[ # ] ")
+
+
+    return utils.display(next_dict)
+
+def download_prescription(next_dict):
+    '''
+    Method corresponding to user choice of downloading prescription.
+    from previous appointment.
+    '''
+    return utils.display(next_dict)
+
+
 
 def book_personal_gp(next_dict):
     '''
@@ -34,44 +131,14 @@ def book_other_gp(next_dict):
 def empty_method(next_dict):
     '''
     Empty method to be stored in the tuple of the dictionary of the user 
-    choice doesn't require any specific steps and just redirects 
+    choice doesn't require any specific steps and just redirects.
     '''
     return utils.display(next_dict)
-
-def manage_appointment(next_dict):
-    '''
-    '''
-    # SQL query qhether the patient has an appointment booked
-    # Hardcoded for now
-    ##################
-    has_appointment = False
-    had_appointment = True
-    #################
-
-    if has_appointment:
-        return utils.display(next_dict["has"])
-
-    elif had_appointment:
-        return utils.display(next_dict["had"])
-
-    else:
-        return utils.display(next_dict["has_not"])
 
 def change_GP_pair(next_dict):
     '''
     '''
     return utils.display(next_dict)
-
-def cancel_appointment(next_dict):
-    '''
-    '''
-    return utils.display(next_dict)
-
-def download_prescription(next_dict):
-    '''
-    '''
-    return utils.display(next_dict)
-
 
 ######################### MENU NAVIGATION DICTIONARIES ######################
 
@@ -94,13 +161,14 @@ flow_1  = {"has_not":
                 "type":"sub",
                 "1":("Yes",empty_method,flow_11)},
           "has":
-                {"title": "MANAGE APPOINTMENT",
+                {"title": "UPCOMING APPOINTMENTS",
                 "type":"sub",
-                "1":("Cancel appointment",cancel_appointment,empty_dict)},
+                "1":("Cancel upcoming appointment",cancel_appointment,empty_dict),
+                "2":("Book new appointment",empty_method,flow_11)},
           "had":
-                {"title": "PAST APPOINTMENT",
+                {"title": "PREVIOUS APPOINTMENTS",
                 "type":"sub",
-                "1":("Download prescription",download_prescription,empty_dict),
+                "1":("Download past appointment prescription",download_prescription,empty_dict),
                 "2":("Book new appointment",empty_method,flow_11)}}
 
 
