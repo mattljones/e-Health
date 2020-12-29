2f711a1a62aac3e13e3a0c0379c4126533a9aaa0
Theo Reignier <theo.reignier.17@ucl.ac.uk>
2020-12-29 17:00:27 +0000

    Error handling patient index input when booking

--- user_menu_flow/patient_flow.py
+++ user_menu_flow/patient_flow.py
@@ -171,6 +171,11 @@ def book_appointment(next_dict):
     # Require user choice of booking slot
     booking_index = input("\n--> ")
 
+    # Formatting user input correctly
+    if booking_index != '#':
+        while len(booking_index) < 3:
+            booking_index = '0' + booking_index
+
     # Build dataframe with only the cell of timeslot selected by user
     # Will return an empty dataframe (nb rows = 0) for '#' and invalid entry
     selected_time_slot = availability[0].where(availability[0]=="["+booking_index+"]").dropna(how='all').dropna(axis=1)
@@ -185,6 +190,12 @@ def book_appointment(next_dict):
         while len(selected_time_slot.index) != 1 and booking_index != '#':
             print("\n\U00002757 Invalid entry, please try again")
             booking_index = input("\n--> ")
+
+            # Formatting user input correctly
+            if booking_index != '#':
+                while len(booking_index) < 3:
+                    booking_index = '0' + booking_index
+
             selected_time_slot = availability[0].where(availability[0]=="["+booking_index+"]").dropna(how='all').dropna(axis=1)
 
         # if user wants to display new availability, recursive call of the function itself
