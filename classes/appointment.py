a47f66a4cb4ceffd4a522d0df5c162912d0170a2
Aleksei Sukhov <aleksei.sukhov.20@ucl.ac.uk>
2020-12-30 20:22:03 +0000

    Appointments.change_status_batch_future changed to the requirements of the Admin Flow

--- classes/appointment.py
+++ classes/appointment.py
@@ -522,7 +522,10 @@ class Appointment:
             u.db_execute(query)
 
     @staticmethod
-    def change_status_batch_future(gp_id, new_status, reject_reason=None):
+    def change_status_batch_future(start_date, end_date, gp_id, new_status, reject_reason=None):
+
+        start_date = dt.datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date = dt.datetime.strptime(end_date, '%Y-%m-%d').date()
 
         if new_status != 'rejected':
             status_update_query = ""
@@ -533,8 +536,8 @@ class Appointment:
         query = """UPDATE booking
                    SET booking_status = '{}'{}
                    WHERE gp_id = {}
-                   AND booking_start_time > '{}'""".format(new_status, status_update_query, gp_id,
-                                                                        dt.datetime.now().strftime("%Y-%m-%d %H:%M"))
+                   AND (date(booking_start_time) BETWEEN '{}' AND '{}')""".format(new_status, status_update_query,
+                                                                                        gp_id, start_date, end_date)
         u.db_execute(query)
 
     # Confirm all of the appointments for a specific GP
@@ -561,6 +564,8 @@ if __name__ == "__main__":
     # Appointment.change_status_batch_future(1, 'rejected')
     # Appointment.change_status(51, 'confirmed')
     # Appointment.change_status(52, 'confirmed')
+
+    # Appointment.change_status_batch_future('2021-01-01', '2021-01-01', 1, 'rejected',"Test")
 
     # print(Appointment.select_GP('week', 16, '2020-12-25')[2])
     # print(Appointment.select_GP('day', 16, '2020-12-25')[3])
