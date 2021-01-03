9db8ac0f3c1a25034edd7d24d08a77f942159ba4
manfredbeluga <manfred.beluga@gmail.com>
2021-01-03 13:31:19 +0100

    implemented password_reset.py into utils.py

--- system/password_reset_test.py
+++ system/password_reset_test.py
@@ -1,11 +1,10 @@
-from system import password_reset as pw_reset
+# from system import password_reset as pw_reset
 from system import utils as u
 
-random_string_password_reset = pw_reset.random_string(8)
+random_string_password_reset = u.random_string(8)
 user_email = 'manuel.buri@gmail.com'
 user_type = 'patient'
-new_password = u.hash_salt('hahafasfa')
 
 # Call after definition of variables above (otherwise one does not know which random_string to enter for validation)
-pw_reset.send_code_to_registered_user(user_type, user_email, random_string_password_reset)
-pw_reset.change_password(user_type, user_email, new_password, random_string_password_reset)
+u.send_code_to_registered_user(user_type, user_email, random_string_password_reset)
+u.change_password(user_type, user_email, random_string_password_reset)
