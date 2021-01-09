6aa5516067b8bd2f8b1eb3d2e168138c61bcec42
Giuseppe Baldini <giuseppebaldini@live.com>
2021-01-09 09:16:05 +0000

    Catching KeyboardInterrupt

--- e_health_main.py
+++ e_health_main.py
@@ -1,5 +1,9 @@
 # e_health_main.py
 
+# Importing built-in modules 
+import os
+import sys
+
 # Importing utility methods from the 'system' package
 from system import globals
 from system import utils
@@ -13,4 +17,13 @@ from user_menu_flow.register_login_flow import main_flow_register
 globals.init()
 print(asciiart.launch_art)
 
-utils.display(main_flow_register)
+try:
+    utils.display(main_flow_register)
+except KeyboardInterrupt:
+    print("\n\n" + "=" * 65 + "\n")
+    print("\U0001F51A THANK YOU FOR USING E-HEALTH. SEE YOU NEXT TIME!")
+    print(asciiart.exit_art)
+    try:
+        sys.exit()
+    except SystemExit:
+        os._exit(0)
