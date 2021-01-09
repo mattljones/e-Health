e05f29e51f14d0ee84dc18aaebe903a1a1eb87ee
Aleksei Sukhov <aleksei.sukhov.20@ucl.ac.uk>
2021-01-09 19:23:11 +0000

    Self sustained python program

--- e_health_main.py
+++ e_health_main.py
@@ -1,5 +1,18 @@
 # e_health_main.py
 
+import pkg_resources
+import subprocess
+import sys
+import os
+required = {'pandas', 'tabulate'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    print('\n\nInstalling "Pandas" and "Tabulate" for a more pleasant user experience!\n\n')
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
+
+
 # Importing utility methods from the 'system' package
 from system import globals
 from system import utils
@@ -10,6 +23,7 @@ from user_menu_flow.register_login_flow import main_flow_register
 
 ############################# MAIN CODE ###############################
 
+os.system('clear')
 globals.init()
 print(asciiart.launch_art)
 
